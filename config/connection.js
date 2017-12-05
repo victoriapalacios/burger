b5b3210b3a2854fe44be24ddb370ce25d97be97a
victoriapalacios <victoria@launchpadlab.com>
2017-12-05 16:34:17 -0600

    configuring for heroku i think

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'hacktheplanet',
+      database: 'todoagain_db'
+    });
+  }
+
 
 // Make connection.
 connection.connect(function(err) {
